aaf3f778372abcf818ba1a153de0ff849655d1e9
trangiabao1908 <hieu40262@gmail.com>
2023-09-09 14:43:12 +0700

    first

---
+++ server/fakeData/index.js
@@ -0,0 +1,16 @@
+export default {
+  authors: [
+    {
+      id: 1,
+      name: "John",
+    },
+  ],
+  folders: [
+    {
+      id: 1,
+      name: "Home",
+      createdAt: "2023",
+      authorId: 1,
+    },
+  ],
+};
