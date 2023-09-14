6367ec3bee079a131d828ab555b6cedf788260ed
trangiabao1908 <hieu40262@gmail.com>
2023-09-14 21:21:03 +0700

    update client url

--- client/src/components/PushNotificationMenu/PushNotificationMenu.jsx
+++ client/src/components/PushNotificationMenu/PushNotificationMenu.jsx
@@ -3,7 +3,7 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import { createClient } from 'graphql-ws';
 import { Badge, Menu, MenuItem } from '@mui/material';
 const client = createClient({
-   url: 'ws://note-app-clone-server-c4f8.onrender.com',
+   url: 'wss://note-app-clone-server-c4f8.onrender.com',
 });
 const query = `subscription Subscription {
   pushNotification {
